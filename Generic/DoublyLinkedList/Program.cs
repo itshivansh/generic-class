1aa9b0c94597f0652707e8429fd26e6ad32a5c9c
shivansh <shivanshakb@gmail.com>
2020-11-04 15:17:58 +0530

    refactored

--- Generic/DoublyLinkedList/Program.cs
+++ Generic/DoublyLinkedList/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoublyLinkedList
 {
-    class Program
+    public class DoublyLinkedList<T>
     {
-        static void Main(string[] args)
+        public int Count { get; set; }
+        List<T> list = new List<T>();
+
+        public void AddFirst(T obj)
         {
-            Console.WriteLine("Hello World!");
+            list.Insert(0, obj);
+            Count = list.Count;
+        }
+        public void AddLast(T objct)
+        {
+            list.Add(objct);
+            Count = list.Count;
+        }
+        public void RemoveFirst()
+        {
+            list.RemoveAt(0);
+            Count--;
+        }
+        public void RemoveLast()
+        {
+            list.RemoveAt(Count - 1);
+            Count--;
         }
     }
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                DoublyLinkedList<object> doublyLinkedList = new DoublyLinkedList<object>();
+                doublyLinkedList.AddFirst("shivansh");
+                doublyLinkedList.AddLast("Tiwari");
+                doublyLinkedList.RemoveFirst();
+                doublyLinkedList.RemoveLast();
+            }
+        }
+    
 }
